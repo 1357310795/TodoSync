ba5ccdbfeb6595fd5021860ef66de777d063dfc3
1357310795 <1357310795@qq.com>
2022-09-24 08:42:01 +0800

    Enhance emoji clean

--- TodoSynchronizer.Core/Extensions/EmojiExtension.cs
+++ TodoSynchronizer.Core/Extensions/EmojiExtension.cs
@@ -10,6 +10,11 @@ namespace TodoSynchronizer.Core.Extensions
     {
         public static string CleanEmoji(this string str)
         {
+            return Clean2(Clean1(str));
+        }
+
+        public static string Clean1(string str)
+        {
             foreach (var a in str)
             {
                 byte[] bts = Encoding.UTF32.GetBytes(a.ToString());
@@ -18,6 +23,22 @@ namespace TodoSynchronizer.Core.Extensions
                 {
                     str = str.Replace(a.ToString(), "");
                 }
+
+            }
+            return str.Trim();
+        }
+
+        public static string Clean2(string str)
+        {
+            StringBuilder sb = new StringBuilder(str.Length);
+            foreach (var a in str)
+            {
+                if (char.GetUnicodeCategory(a) == System.Globalization.UnicodeCategory.OtherSymbol || char.GetUnicodeCategory(a) == System.Globalization.UnicodeCategory.Surrogate)
+                {
+
+                }
+                else
+                   sb.Append(a);
 
             }
             return str.Trim();
