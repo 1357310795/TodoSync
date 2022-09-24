d5f68ec7a76f92a3467063796470d0bf00623b71
1357310795 <1357310795@qq.com>
2022-09-25 07:41:57 +0800

    Fix timezone issue again

--- TodoSynchronizer.Core/Helpers/CanvasPreference.cs
+++ TodoSynchronizer.Core/Helpers/CanvasPreference.cs
@@ -32,11 +32,11 @@ namespace TodoSynchronizer.Core.Helpers
             switch (SyncConfig.Default.QuizConfig.RemindMode)
             {
                 case RemindMode.unlock_at:
-                    return quiz.UnlockAt?.AddHours(8);
+                    return quiz.UnlockAt;
                 case RemindMode.before_due_at:
-                    return quiz.DueAt == null ? null : quiz.DueAt?.AddHours(8) - SyncConfig.Default.QuizConfig.BeforeTimeSpan;
+                    return quiz.DueAt == null ? null : quiz.DueAt - SyncConfig.Default.QuizConfig.BeforeTimeSpan;
                 case RemindMode.before_lock_at:
-                    return quiz.LockAt == null ? null : quiz.LockAt?.AddHours(8) - SyncConfig.Default.QuizConfig.BeforeTimeSpan;
+                    return quiz.LockAt == null ? null : quiz.LockAt - SyncConfig.Default.QuizConfig.BeforeTimeSpan;
                 default:
                     return null;
             }
@@ -47,9 +47,9 @@ namespace TodoSynchronizer.Core.Helpers
             switch (SyncConfig.Default.QuizConfig.DueDateMode)
             {
                 case DueDateMode.due_at:
-                    return quiz.DueAt?.AddHours(8);
+                    return quiz.DueAt;
                 case DueDateMode.lock_at:
-                    return quiz.LockAt?.AddHours(8);
+                    return quiz.LockAt;
                 default:
                     return null;
             }
@@ -62,11 +62,11 @@ namespace TodoSynchronizer.Core.Helpers
             switch (SyncConfig.Default.AssignmentConfig.RemindMode)
             {
                 case RemindMode.unlock_at:
-                    return assignment.UnlockAt?.AddHours(8);
+                    return assignment.UnlockAt;
                 case RemindMode.before_due_at:
-                    return assignment.DueAt == null ? null : assignment.DueAt?.AddHours(8) - SyncConfig.Default.AssignmentConfig.BeforeTimeSpan;
+                    return assignment.DueAt == null ? null : assignment.DueAt - SyncConfig.Default.AssignmentConfig.BeforeTimeSpan;
                 case RemindMode.before_lock_at:
-                    return assignment.LockAt == null ? null : assignment.LockAt?.AddHours(8) - SyncConfig.Default.AssignmentConfig.BeforeTimeSpan;
+                    return assignment.LockAt == null ? null : assignment.LockAt - SyncConfig.Default.AssignmentConfig.BeforeTimeSpan;
                 default:
                     return null;
             }
@@ -77,9 +77,9 @@ namespace TodoSynchronizer.Core.Helpers
             switch (SyncConfig.Default.AssignmentConfig.DueDateMode)
             {
                 case DueDateMode.due_at:
-                    return assignment.DueAt?.AddHours(8);
+                    return assignment.DueAt;
                 case DueDateMode.lock_at:
-                    return assignment.LockAt?.AddHours(8);
+                    return assignment.LockAt;
                 default:
                     return null;
             }
@@ -94,7 +94,7 @@ namespace TodoSynchronizer.Core.Helpers
 
         public static DateTime? GetDueTime(this Anouncement anouncement)
         {
-            return anouncement.PostedAt?.AddHours(8);
+            return anouncement.PostedAt;
         }
         #endregion
 
@@ -106,7 +106,7 @@ namespace TodoSynchronizer.Core.Helpers
 
         public static DateTime? GetDueTime(this Discussion discussion)
         {
-            return discussion.PostedAt?.AddHours(8);
+            return discussion.PostedAt;
         }
         #endregion
 
@@ -118,7 +118,7 @@ namespace TodoSynchronizer.Core.Helpers
 
         public static DateTime? GetDueTime(this Notification notification)
         {
-            return SyncConfig.Default.NotificationConfig.DueDateMode == DueDateMode.start_at? notification.StartAt?.AddHours(8) : notification.EndAt?.AddHours(8);
+            return SyncConfig.Default.NotificationConfig.DueDateMode == DueDateMode.start_at? notification.StartAt : notification.EndAt;
         }
         #endregion
 
